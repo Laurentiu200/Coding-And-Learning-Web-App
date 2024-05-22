189911b62e2f509393b83f976d3d18343a235f96
Laurentiu200 <74619400+Laurentiu200@users.noreply.github.com>
2024-05-22 13:08:23 +0300

    Add files via upload

---
+++ Coding_And_Learning_Web_App/src/main/java/com/example/coding_and_learning_web_app/CodingAndLearningWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.coding_and_learning_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingAndLearningWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodingAndLearningWebAppApplication.class, args);
+    }
+
+}
---
+++ Coding_And_Learning_Web_App/src/test/java/com/example/coding_and_learning_web_app/CodingAndLearningWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coding_and_learning_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingAndLearningWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
